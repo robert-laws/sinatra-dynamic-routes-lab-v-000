5f85a1e70559ff4fa22c1b6ec54c7fb3dad4c2e9
Robert Laws <roblaws@me.com>
2017-10-25 07:55:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,8 +18,15 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     @phrase = params[:phrase]
     @number.times { "#{@phrase}" }
-    
+
   end
 
-  
+  get "/say/:word1:/:word2/:word3/:word4/:word5" do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
+  end
 end
